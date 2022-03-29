35ca0850f9512c5770977a720306e801a48fc4a1
ryo.matsuzaki <matsuzaki.eg33@members.co.jp>
2022-03-29 13:55:38 +0900

    add mixed test case

--- main_test.go
+++ main_test.go
@@ -116,3 +116,67 @@ func ExampleReplaceChange() {
 	//
 	// </details>
 }
+
+func ExampleAllMixed() {
+	RunTestMain("test_data/all_mixed/show.json")
+	// Output:
+	// ### 2 to add, 1 to change, 2 to destroy.
+	// - add
+	// 	- env_variable.test5
+	// - change
+	// 	- env_variable.test2
+	// - destroy
+	// 	- env_variable.test3
+	// - replace
+	// 	- random_id.test4
+	// <details><summary>Change details (Click me)</summary>
+	//
+	// ```diff
+	// resource env_variable test2
+	// @@ -1,5 +1,5 @@
+	//  {
+	//    "id": "test2",
+	// -  "name": "test2",
+	// +  "name": "test2_changed",
+	//    "value": ""
+	//  }
+	// ```
+	//
+	// ```diff
+	// resource env_variable test3
+	// @@ -1,5 +1 @@
+	// -{
+	// -  "id": "test3",
+	// -  "name": "test3",
+	// -  "value": ""
+	// -}
+	// +null
+	// ```
+	//
+	// ```diff
+	// resource env_variable test5
+	// @@ -1 +1,3 @@
+	// -null
+	// +{
+	// +  "name": "test5"
+	// +}
+	// ```
+	//
+	// ```diff
+	// resource random_id test4
+	// @@ -1,10 +1,5 @@
+	//  {
+	// -  "b64_std": "m6S5W82/OFA=",
+	// -  "b64_url": "m6S5W82_OFA",
+	// -  "byte_length": 8,
+	// -  "dec": "11215292776004401232",
+	// -  "hex": "9ba4b95bcdbf3850",
+	// -  "id": "m6S5W82_OFA",
+	// +  "byte_length": 10,
+	//    "keepers": null,
+	//    "prefix": null
+	//  }
+	// ```
+	//
+	// </details>
+}
