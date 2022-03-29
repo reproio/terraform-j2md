13bf3f190de35b001fd6d0fc6007c3f19133d3dd
ryo.matsuzaki <matsuzaki.eg33@members.co.jp>
2022-03-29 12:13:09 +0900

    fix tests to adapt to app implementation

--- main_test.go
+++ main_test.go
@@ -14,7 +14,6 @@ func RunTestMain(inputFile string) {
 	input, err := os.Open(inputFile)
 	if err != nil {
 		log.Fatalf("cannot open input file: %s", inputFile)
-		return
 	}
 
 	defer input.Close()
@@ -28,39 +27,91 @@ func ExampleNoChanges() {
 	// 0 to add, 0 to change, 0 to destroy.
 }
 
-func ExampleAdd() {
+func ExampleSingleAdd() {
 	RunTestMain("test_data/single_add/show.json")
 	// Output:
-	// 1 to add, 0 to change, 0 to destroy.
-	//
-	// - add:
-	//   - null_resource.foo
-	//
+	// ### 1 to add, 0 to change, 0 to destroy.
+	// - add
+	// 	- null_resource.foo
 	// <details><summary>Change details (Click me)</summary>
 	//
 	// ```diff
-	// + resource "null_resource" "foo" {
-	// +   id = (known after apply)
-	// +  }
+	// resource null_resource foo
+	// @@ -1 +1,3 @@
+	// -null
+	// +{
+	// +  "triggers": null
+	// +}
 	// ```
 	//
 	// </details>
 }
 
-func ExampleDestroy() {
+func ExampleSingleDestroy() {
 	RunTestMain("test_data/single_destroy/show.json")
 	// Output:
-	// 0 to add, 0 to change, 1 to destroy.
-	//
-	// - destroy:
-	//   - null_resource.foo
-	//
+	// ### 0 to add, 0 to change, 1 to destroy.
+	// - destroy
+	// 	- null_resource.foo
 	// <details><summary>Change details (Click me)</summary>
 	//
 	// ```diff
-	// - resource "null_resource" "foo" {
-	// -   id = "317876227733854172"
-	// -  }
+	// resource null_resource foo
+	// @@ -1,4 +1 @@
+	// -{
+	// -  "id": "317876227733854172",
+	// -  "triggers": null
+	// -}
+	// +null
+	// ```
+	//
+	// </details>
+}
+
+func ExampleSingleChange() {
+	RunTestMain("test_data/single_change/show.json")
+	// Output:
+	// 	### 0 to add, 1 to change, 0 to destroy.
+	// - change
+	//	- env_variable.test1
+	// <details><summary>Change details (Click me)</summary>
+	//
+	// ```diff
+	// resource env_variable test1
+	// @@ -1,5 +1,5 @@
+	//  {
+	//    "id": "test1",
+	// -  "name": "test1",
+	// +  "name": "test1_changed",
+	//    "value": ""
+	//  }
+	// ```
+	//
+	// </details>
+}
+
+func ExampleReplaceChange() {
+	RunTestMain("test_data/single_replace/show.json")
+	// Output:
+	// ### 1 to add, 0 to change, 1 to destroy.
+	// - replace
+	// 	- random_id.test
+	// <details><summary>Change details (Click me)</summary>
+	//
+	// ```diff
+	// resource random_id test
+	// @@ -1,10 +1,5 @@
+	//  {
+	// -  "b64_std": "qddo6VPNl1g=",
+	// -  "b64_url": "qddo6VPNl1g",
+	// -  "byte_length": 8,
+	// -  "dec": "12238365863745263448",
+	// -  "hex": "a9d768e953cd9758",
+	// -  "id": "qddo6VPNl1g",
+	// +  "byte_length": 10,
+	//    "keepers": null,
+	//    "prefix": null
+	//  }
 	// ```
 	//
 	// </details>
