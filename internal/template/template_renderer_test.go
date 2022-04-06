b90b1060625bc9eff08e88b8b6c47cffa05195a9
ryo.matsuzaki <matsuzaki.eg33@members.co.jp>
2022-04-06 11:07:16 +0900

    fix testdata pathes

--- internal/template/template_renderer_test.go
+++ internal/template/template_renderer_test.go
@@ -1,4 +1,4 @@
-package main
+package template
 
 import (
 	"os"
@@ -14,15 +14,15 @@ func Test_render(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		{name: "No Changes", args: args{testDataPath: "testdata/no_changes"}, wantErr: false},
-		{name: "Single Create", args: args{testDataPath: "testdata/single_add"}, wantErr: false},
-		{name: "Single Change", args: args{testDataPath: "testdata/single_change"}, wantErr: false},
-		{name: "Single Destroy", args: args{testDataPath: "testdata/single_destroy"}, wantErr: false},
-		{name: "Single Replace", args: args{testDataPath: "testdata/single_replace"}, wantErr: false},
-		{name: "All Change Types Mixed", args: args{testDataPath: "testdata/all_mixed"}, wantErr: false},
-		{name: "AWS Resource Changes", args: args{testDataPath: "testdata/aws_mixed"}, wantErr: false},
-		{name: "Invalid JSON input", args: args{testDataPath: "testdata/invalid_json"}, wantErr: true},
-		{name: "Invalid format input", args: args{testDataPath: "testdata/not_json"}, wantErr: true},
+		{name: "No Changes", args: args{testDataPath: "../../test/testdata/no_changes"}, wantErr: false},
+		{name: "Single Create", args: args{testDataPath: "../../test/testdata/single_add"}, wantErr: false},
+		{name: "Single Change", args: args{testDataPath: "../../test/testdata/single_change"}, wantErr: false},
+		{name: "Single Destroy", args: args{testDataPath: "../../test/testdata/single_destroy"}, wantErr: false},
+		{name: "Single Replace", args: args{testDataPath: "../../test/testdata/single_replace"}, wantErr: false},
+		{name: "All Change Types Mixed", args: args{testDataPath: "../../test/testdata/all_mixed"}, wantErr: false},
+		{name: "AWS Resource Changes", args: args{testDataPath: "../../test/testdata/aws_mixed"}, wantErr: false},
+		{name: "Invalid JSON input", args: args{testDataPath: "../../test/testdata/invalid_json"}, wantErr: true},
+		{name: "Invalid format input", args: args{testDataPath: "../../test/testdata/not_json"}, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
