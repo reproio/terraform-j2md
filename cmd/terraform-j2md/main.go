c1d04a1a3c6ce3616c425bb1300c822cf1f4e745
Alexander.Bij <alexander.bij@rabobank.nl>
2023-02-20 09:36:15 +0100

    New pr, no need to change whitespares

--- cmd/terraform-j2md/main.go
+++ cmd/terraform-j2md/main.go
@@ -18,13 +18,11 @@ func run() int {
 		fmt.Fprintf(os.Stderr, "cannot read stdin: %v", err)
 		return 1
 	}
-
 	planData, err := terraform.NewPlanData(input)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "cannot parse input as Terraform plan JSON: %v", err)
 		return 1
 	}
-
 	if err = planData.Render(os.Stdout); err != nil {
 		fmt.Fprintf(os.Stderr, "cannot render: %v", err)
 		return 1
